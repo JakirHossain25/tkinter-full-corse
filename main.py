107e151452a36eda11569714de6d7ca7f3ec5050
JakirHossain25 <sarkerjakariya5@gmail.com>
2025-11-02 14:15:38 +0600

    this is for laben frame

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+# label frame widget #
+from tkinter import*
+win=Tk()
+win.title("label frame windget")
+win.geometry("450x450")
+win.config(bg='green')
+
+label=LabelFrame(win,text="label frame",bg="green",font=(5),labelanchor=N)
+label.place(x=100,y=100,width=300,height=100)
+
+
+
+win.mainloop()
+
 
