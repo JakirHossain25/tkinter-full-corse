bdda5955bd3b65742a4109ce4ef9968d1d3033ab
JakirHossain25 <sarkerjakariya5@gmail.com>
2025-11-02 16:23:38 +0600

    this is for chscke buttom

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-# this is for buton#
+# chak bocx funtion#
 from tkinter import*
+
+def x():
+    print(var.get())
+    
 win=Tk()
-win.title("this is for button")
+win.title("this is for chakbox")
 win.geometry("600x600")
 
-bt=Button(win,text="shop now",bg="green",fg="white",bd="3",relief="groove")
-bt.place(x=100,y=100,width=100,height=50)
+var=BooleanVar()
 
+cb=Checkbutton(win,text="shop now",bg="green",fg="black",bd="3",relief="groove",variable=var)
+cb.pack()
+
+b=Button(win,text="shop now",bg="green",fg="black",bd="3",relief="groove",command=x)
+b.pack()
+
+print(var.get())
 
 win.mainloop()
